8d586edc7da761a88fc009a56019e6ca0eb3dd7a
perlancar (on netbook-zenbook-ux305) <perlancar@gmail.com>
2017-07-04 16:06:40 +0700

    Initial commit

---
+++ lib/Module/Load/In/INIT.pm
@@ -0,0 +1,56 @@
+package Module::Load::In::INIT;
+
+# DATE
+# VERSION
+
+my @mods;
+
+sub import {
+    my $pkg = shift;
+
+    push @mods, @_;
+}
+
+INIT {
+    for my $mod (@mods) {
+        my $mod_pm;
+        if ($mod =~ m!/!) {
+            $mod_pm = $mod;
+        } else {
+            ($mod_pm = "$mod.pm") =~ s!::!/!g;
+        }
+        require $mod_pm;
+    }
+}
+
+1;
+
+#ABSTRACT: Load modules in INIT phase
+
+=head1 SYNOPSIS
+
+In the command-line:
+
+ % perl -MModule::Load::In::INIT=Mod::One,Mod::Two somescript.pl
+
+C<Mod::One> and C<Mod::Two> will be loaded in the INIT phase instead of BEGIN
+phase.
+
+
+=head1 DESCRIPTION
+
+This module can load (or perhaps defer loading) modules in the INIT phase
+instead of the BEGIN phase. One use-case where it is useful: monkey-patching a
+module (using a L<Module::Patch>-based module) in a fatpacked script (see
+L<Module::FatPack> or L<App::FatPacker>), e.g.:
+
+ % perl -MSome::Module::Patch::Foo fatpacked-script.pl
+
+C<Some::Module::Patch::Foo> will try to load C<Some::Module> then patch it. This
+will fail when module is loaded by the fatpack handler (which is a require hook)
+as by the time C<Some::Module::Patch::Foo> is loaded, the fatpack handler has
+not been setup yet. This, however, works:
+
+ % perl -MModule::Load::In::INIT=Some::Module::Patch::Foo fatpacked-script.pl
+
+=cut
